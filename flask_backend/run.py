16ba6142a7e725dde49e00d96fa5452a3c4ed90f
Manu <manu.m@21north.in>
2020-05-30 01:23:05 +0530

    Latest code

--- flask_backend/run.py
+++ flask_backend/run.py
@@ -1,5 +1,5 @@
 from app import app
-app.run(host='127.0.0.1', port=8080, debug=True)
+app.run(host='192.168.43.114', port=8080, debug=True)
 
 # To call the server endpoint through android emulator
 # app.run(host='0.0.0.0', port=8000, debug=True)
