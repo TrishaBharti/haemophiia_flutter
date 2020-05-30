e9e431041cede8d9d42062687cfd45be18ef0dca
trisha <trishabharti.mca17@rvce.edu.in>
2020-05-30 17:12:40 +0530

    Latest Code

--- flask_backend/config.py
+++ flask_backend/config.py
@@ -2,5 +2,5 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:manu1995@localhost/haemophilia'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pass@localhost/haemo'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
