abc90261185d441b37216c62305884182f37f2b6
trisha <trishabharti.mca17@rvce.edu.in>
2020-05-30 17:11:29 +0530

    Latest

--- flutter_login-master/lib/services/flask_services.dart
+++ flutter_login-master/lib/services/flask_services.dart
@@ -1,9 +1,12 @@
 import 'package:requests/requests.dart';
 import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
 class FlaskServices{
 
   static void addPatient(name, phoneNumber, bloodGroup, factorD, factorA) async{
+
 
     var requestBody = {
       "name":name,
@@ -13,14 +16,16 @@ class FlaskServices{
       "factor_assay":factorA
     };
 
-    var response = await Requests.post("http://192.168.43.114:8080/v1/add-patient",
+    var response = await Requests.post("http://192.168.43.208:8000/v1/add-patient",
       bodyEncoding: RequestBodyEncoding.JSON,
       body: requestBody
     );
+    
+
   }  
 
   static Future getPatients() async {
-    var response = await Requests.get("http://192.168.43.114:8080/v1/get-patient-details");
+    var response = await Requests.get("http://192.168.43.208:8000/v1/get-patient-details");
     String body = response.content();
     Map<String, dynamic> myMap = json.decode(body);
     List jsonData = myMap['data'];
@@ -29,7 +34,7 @@ class FlaskServices{
 
   static Future getPatientDetails(id) async {
     
-    var response = await Requests.get("http://192.168.43.114:8080/v1/get-patient-details-by-id/" + id);
+    var response = await Requests.get("http://192.168.43.208:8000/v1/get-patient-details-by-id/" + id);
     String body = response.content();
     Map<String, dynamic> myMap = json.decode(body);
     List jsonData = myMap['data'];
@@ -42,7 +47,7 @@ class FlaskServices{
       "request_type":"getbedetails",
 	    "p_id":id
     };
-    var response = await Requests.post("http://192.168.43.114:8080/v1/get-be",
+    var response = await Requests.post("http://192.168.43.208:8000/v1/get-be",
       body: requestBody,
       bodyEncoding: RequestBodyEncoding.JSON,
     );
@@ -63,7 +68,7 @@ class FlaskServices{
       "request_type":"getfishdetails",
 	    "p_id":id
     };
-    var response = await Requests.post("http://192.168.43.114:8080/v1/get-fish",
+    var response = await Requests.post("http://192.168.43.208:8000/v1/get-fish",
       body: requestBody,
       bodyEncoding: RequestBodyEncoding.JSON,
     );
@@ -81,7 +86,7 @@ class FlaskServices{
 
   static Future addbeDetails(requestBody) async {
 
-    var response = await Requests.post("http://192.168.43.114:8080/v1/add-be",
+    var response = await Requests.post("http://192.168.43.208:8000/v1/add-be",
       body: requestBody, 
       bodyEncoding: RequestBodyEncoding.JSON
     );
@@ -92,7 +97,7 @@ class FlaskServices{
 
   static Future addFishDetails(requestBody) async {
 
-    var response = await Requests.post("http://192.168.43.114:8080/v1/add-fish",
+    var response = await Requests.post("http://192.168.43.208:8000/v1/add-fish",
       body: requestBody, 
       bodyEncoding: RequestBodyEncoding.JSON
     );
